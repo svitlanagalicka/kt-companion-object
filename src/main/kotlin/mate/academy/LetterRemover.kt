4fd80a18c878c631b08d968a62f37334796ba08d
Svitlana Galicka <svitlanakozak12332@gmail.com>
2025-10-19 20:58:57 +0300

    replaced r and !

--- src/main/kotlin/mate/academy/LetterRemover.kt
+++ src/main/kotlin/mate/academy/LetterRemover.kt
@@ -1,5 +1,16 @@
 package mate.academy
 
-class LetterRemover {
-    // implement ...
+object LetterRemover {
+    @JvmStatic
+    fun removeLetter(input: String): CharArray {
+        val lowercase = input.lowercase()
+        val replace = lowercase.replace('r', '!')
+        return replace.toCharArray()
+    }
+}
+
+fun main() {
+    val input = "Remove Letters"
+    val result = LetterRemover.removeLetter(input)
+    println(result.contentToString())
 }
